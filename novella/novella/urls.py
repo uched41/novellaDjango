2d0e0a37ee08de1013861ee1e1ea9877348b7601
uched41 <uched41@yahoo.com>
2019-03-13 21:48:34 +0000

    Added haproxy, hostame support

--- novella/novella/urls.py
+++ novella/novella/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^main/', include('main.urls')),
+    url(r'^', include('main.urls')),
 ]
--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
-sudo docker-compose up
+#!/bin/sh
+sleep 30
+echo "Starting init script:"
+date
+sudo service mosquitto stop
+cd /home/novellamaker/novellaDjango/
+echo "Starting docker" "
+sudo docker-compose up -d
